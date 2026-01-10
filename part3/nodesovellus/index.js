10aaa4e8efb5e8a52d1590547dcc4f1c423e815c
Mollbjo <joonamollberg@gmail.com>
2026-01-10 17:32:45 +0200

    Finalize frontend and bacnend compatability and remove cors from backend

--- part3/nodesovellus/index.js
+++ part3/nodesovellus/index.js
@@ -1,14 +1,10 @@
 const express = require('express')
 
-const cors = require('cors')
-
 var morgan = require('morgan')
 
 const app = express()
 
 app.use(express.json())
-
-app.use(cors())
 
 app.use(express.static('dist'))
 
